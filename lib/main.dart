d871069139f8ad935edcd5ea6d1544b0ea9d413e
william <wmmnpr@gmail.com>
2024-07-28 08:34:10 +0200

    add rowing animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'segment_display.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RegattaManager {
+  static final List<Boat> _boats = initBoats();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  static List<Boat> initBoats() {
+    List<Boat> list = [];
+    list.add(new Boat(" 900", 0));
+    list.add(new Boat("1000", 200));
+    list.add(new Boat("1000", 210));
+    return list;
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Boat {
+  String distance;
+  double rowed = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Boat(this.distance, this.rowed);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class RowingScene extends CustomPainter {
+  RegattaManager boatManager = RegattaManager();
 
-  void _incrementCounter() {
+  Paint bgPaint = Paint().configure(color: Colors.lightBlue);
+
+  Paint linePaint = Paint().configure(color: Colors.yellow, strokeWidth: 4.0);
+  Paint endLine = Paint().configure(color: Colors.white, strokeWidth: 4.0);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Rect rectSize =
+        Rect.fromPoints(Offset(0, 0), Offset(size.width, size.height));
+    canvas.drawRect(rectSize, bgPaint);
+
+    List<Boat> boats = RegattaManager._boats;
+
+    int laneCount = boats.length;
+    double lh = size.height / laneCount;
+    double lw = size.width;
+    for (int i = 0; i < laneCount; i++) {
+      Boat b = boats[i];
+      Offset p1 = Offset(0, lh * i); //top
+      Offset p2 = Offset(lw, lh * i); //bottom
+      canvas.drawLine(p1, p2, linePaint);
+
+      p1 = Offset(lw / 100, lh * i + 50); //start
+      p2 = Offset(lw / 100, lh * (i + 1) - 50);
+      canvas.drawLine(p1, p2, endLine);
+
+      p1 = Offset(lw - lw / 100, lh * i + 50); //end
+      p2 = Offset(lw - lw / 100, lh * (i + 1) - 50);
+      canvas.drawLine(p1, p2, endLine);
+
+      SegmentDisplay sg2 = SegmentDisplay(
+          canvas, lw / 2.0 - 200 / 2.0, lh * i + lh / 4.0, 200, 50, b.distance);
+      sg2.draw();
+
+      Rect r = Rect.fromLTWH(b.rowed - 50, lh * i + lh / 2.0 - 25, 100, 40);
+
+      canvas.drawRect(r, this.linePaint);
+    }
+    Offset p1 = Offset(0, size.height); //top
+    Offset p2 = Offset(size.width, size.height); //bottom
+    canvas.drawLine(p1, p2, linePaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    print("shouldRepaint RowingScene ***********************");
+    return true;
+  }
+}
+
+class RowingSceneWidget extends StatefulWidget {
+  const RowingSceneWidget({super.key});
+
+  @override
+  _RowingSceneWidgetState createState() => _RowingSceneWidgetState();
+}
+
+class _RowingSceneWidgetState extends State<RowingSceneWidget> {
+  int pushCount = 0;
+
+  void _updateBoats() {
+    pushCount++;
+    RegattaManager._boats.forEach((b) => b.rowed = b.rowed + 10);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      // Trigger a repaint by updating the state
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: CustomPaint(
+          painter: RowingScene(),
+          child: Container(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _updateBoats,
+        child: Icon(Icons.refresh),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: RowingSceneWidget(),
+  ));
 }
